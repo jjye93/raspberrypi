8b9fc463e34a31c676afbb66561ecf8f1716ab9c
jjye93 <89705425+jjye93@users.noreply.github.com>
2024-12-15 15:16:28 +0800

    Create docker.sh

---
+++ docker.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/bash
+set -e
+
+echo "----------------------------------------------------------------"
+echo "Commence Docker Setup"
+echo "----------------------------------------------------------------"
+sudo apt-get install -y docker.io docker-compose || handle_error
+echo "----------------------------------------------------------------"
+sudo usermod -aG docker admin || handle_error "Adding user to Docker group"
+sudo systemctl enable docker.service || handle_error "Enabling Docker service"
+sudo systemctl start docker.service || handle_error "Starting Docker service"
+
+echo "----------------------------------------------------------------"
+echo "Docker Setup Completed"
+echo "----------------------------------------------------------------"
+echo "Calling Samba Setup..."
+curl https://
