fff8ea10cffde13fa457f2b8849c2b9e65b1d395
jjye93 <89705425+jjye93@users.noreply.github.com>
2024-12-15 15:17:00 +0800

    Create glances.sh

---
+++ glances.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/bash
+set -e
+echo "----------------------------------------------------------------"
+echo "Commence Glances Setup"
+echo "----------------------------------------------------------------"
+sudo apt-get install glances -y || handle_error
+sudo systemctl enable glances || handle_error
+sudo systemctl start glances || handle_error
+echo "----------------------------------------------------------------"
+echo "Glances setting completed and host at http://localhost:61208"
+echo "----------------------------------------------------------------"
+echo " "
+echo "Calling root-ssh account Setup"
+curl https://
