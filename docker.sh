db5416ab428bb9cdecef84b743982e96d43dcf06
jjye93 <89705425+jjye93@users.noreply.github.com>
2024-12-15 15:44:07 +0800

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -14,4 +14,4 @@ echo "----------------------------------------------------------------"
 echo "Docker Setup Completed"
 echo "----------------------------------------------------------------"
 echo "Calling Samba Setup..."
-curl https://
+curl https://raw.githubusercontent.com/jjye93/config-file/refs/heads/breakdown-script/samba.sh | sudo bash
