61f218f77c3baf7b0a733401908fed06ff6f6a6c
jjye93 <89705425+jjye93@users.noreply.github.com>
2024-12-15 15:45:20 +0800

    Update system_update.sh

--- system_update.sh
+++ system_update.sh
@@ -11,4 +11,4 @@ echo "----------------------------------------------------------------"
 
 # Call next script
 echo "Calling Argon Setup..."
-curl -fsSL 
+curl https://raw.githubusercontent.com/jjye93/config-file/refs/heads/breakdown-script/argon.sh | sudo bash
