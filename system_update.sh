fcfdf48631f1d5df6e721ed960ea1f3aee1e6620
jjye93 <89705425+jjye93@users.noreply.github.com>
2024-12-21 02:41:54 +0800

    Update system_update.sh

--- system_update.sh
+++ system_update.sh
@@ -8,7 +8,3 @@ sudo apt-get update && sudo apt-get upgrade -y || handle_error "System Upgrade"
 echo "----------------------------------------------------------------"
 echo "System Upgrade Completed"
 echo "----------------------------------------------------------------"
-
-# Call next script
-echo "Calling Argon Setup..."
-curl https://raw.githubusercontent.com/jjye93/config-file/refs/heads/breakdown-script/argon.sh | sudo bash
