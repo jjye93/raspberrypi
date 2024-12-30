d2f2380badffb93cfe0f12642f23c957e494947a
jjye93 <89705425+jjye93@users.noreply.github.com>
2024-12-30 09:45:21 +0800

    Update argon.sh

--- argon.sh
+++ argon.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/bash
-set -e
 
 echo "----------------------------------------------------------------"
 echo "Commence Argon One Setup"
 echo "----------------------------------------------------------------"
-curl https://download.argon40.com/argon1.sh | bash || handle_error "Argon One Setup"
+curl https://download.argon40.com/argon1.sh | bash
 echo "----------------------------------------------------------------"
 echo "Argon One Setup Completed Successfully"
 echo "----------------------------------------------------------------"
