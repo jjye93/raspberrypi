e946da7e6544f8e598b32c9d8d4db983d40b3fec
jjye93 <89705425+jjye93@users.noreply.github.com>
2025-01-02 22:57:17 +0800

    Update samba.sh

--- samba.sh
+++ samba.sh
@@ -1,26 +1,24 @@
 #!/usr/bin/bash
-set -e
-
 echo "----------------------------------------------------------------"
 echo "Commence Samba Setup"
 echo "----------------------------------------------------------------"
 echo "Installing Samba..."
-sudo apt-get install -y samba || handle_error
+sudo apt-get install -y samba 
 echo "Stopping Samba service..."
-sudo systemctl stop samba || handle_error
+sudo systemctl stop samba 
 echo "Backup smb.conf"
-sudo mv /etc/samba/smb.conf /etc/samba/smb.conf.1 || handle_error
-sudo wget -O /etc/samba/smb.conf https://raw.githubusercontent.com/jjye93/config-file/refs/heads/main/smb.conf ||handle_error
+sudo mv /etc/samba/smb.conf /etc/samba/smb.conf.1 
+sudo wget -O /etc/samba/smb.conf https://raw.githubusercontent.com/jjye93/config-file/refs/heads/main/smb.conf
 echo "Setting correct permissions for smb.conf..."
-sudo chmod 644 /etc/samba/smb.conf || handle_error
-sudo chown root:root /etc/samba/smb.conf || handle_error
+sudo chmod 644 /etc/samba/smb.conf
+sudo chown root:root /etc/samba/smb.conf
 echo "Starting Samba service..."
-sudo systemctl start samba || handle_error
+sudo systemctl start samba 
 echo "Setting Samba password for user 'admin'..."
 if ! command -v expect &> /dev/null
 then
     echo "Expect is not installed, installing..."
-    sudo apt-get install -y expect || handle_error
+    sudo apt-get install -y expect
 fi
 
 expect <<EOF
@@ -32,7 +30,7 @@ send "admin\r"
 expect eof
 EOF
 echo "Restarting Samba service..."
-sudo systemctl restart samba || handle_error
+sudo systemctl restart samba
 echo "----------------------------------------------------------------"
 echo "Samba configuration is complete. SMB user 'admin' has been created with the password 'admin'."
 echo "----------------------------------------------------------------"
