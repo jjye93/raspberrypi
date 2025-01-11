c105eda43280183d00c71e9dbd195f142b26e8da
jjye93 <89705425+jjye93@users.noreply.github.com>
2025-01-12 02:33:03 +0800

    Create install.sh

---
+++ raspberrypi/argonone/install.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/bash
+echo "argon-one"
+curl https://download.argon40.com/argon-eeprom.sh | bash
+curl https://download.argon40.com/argon1.sh | bash 
+echo "argon-config"
+echo "complete"
