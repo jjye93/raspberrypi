8ae0cb48b031885272a7925bcff142e7e4090dc2
jjye93 <89705425+jjye93@users.noreply.github.com>
2025-01-15 01:51:26 +0800

    Update install.sh

--- raspberrypi/aria2/install.sh
+++ raspberrypi/aria2/install.sh
@@ -6,12 +6,12 @@ sudo mkdir /etc/aria2-webui
 sudo git clone https://github.com/ziahamza/webui-aria2.git /etc/aria2-webui
 sudo killall aria2c
 sudo mkdir /etc/aria2 -p
-sudo wget -O /etc/aria2/aria.conf https://raw.githubusercontent.com/jjye93/config-file/refs/heads/main/raspberrypi/aria2/aria2.conf
+sudo wget -O /etc/aria2/aria2.conf https://raw.githubusercontent.com/jjye93/config-file/refs/heads/main/raspberrypi/aria2/aria2.conf
 sudo chmod 755 /etc/aria2/aria2.conf
 sudo wget -O /etc/systemd/system/aria2c.service https://raw.githubusercontent.com/jjye93/config-file/refs/heads/main/raspberrypi/aria2/aria2c.service
 sudo chmod 755 /etc/systemd/system/aria2c.service
-sudo systemctl enable aria2
-sudo systemctl start aria2
+sudo systemctl enable aria2c
+sudo systemctl start aria2c
 git clone https://github.com/ziahamza/webui-aria2.git
 sudo mv webui-aria2 /etc/
 cd /etc/webui-aria2
