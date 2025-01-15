2394881142495e3f739e908f98a516651fe4c28d
jjye93 <89705425+jjye93@users.noreply.github.com>
2025-01-15 20:46:31 +0800

    Update install.sh

--- smart home/mosquitto/install.sh
+++ smart home/mosquitto/install.sh
@@ -3,9 +3,9 @@ echo "mosquitto"
 sudo apt-get update && sudo apt-get full-upgrade -y
 sudo apt-get install mosquitto -y
 sudo systemctl stop mosquitto
-sudo mkdir -p /etc/mosquitto
-sudo wget -o /etc/mosquitto/mosquitto.conf https://raw.githubusercontent.com/jjye93/config-file/refs/heads/main/smart%20home/mosquitto/mosquitto.conf
-sudo chmod 777 /etc/mosquitto/mosquitto.conf
+sudo mkdir -p /opt/mosquitto
+sudo wget -o /opt/mosquitto/mosquitto.conf https://raw.githubusercontent.com/jjye93/config-file/refs/heads/main/smart%20home/mosquitto/mosquitto.conf
+sudo chmod 777 /opt/mosquitto/mosquitto.conf
 sudo systemctl enable mosquitto.conf
 mosquitto -v
 echo "complete"
