2fe0ded5d48f7f3bb39f1215a32653be4765b1ec
jjye93 <89705425+jjye93@users.noreply.github.com>
2025-01-24 03:48:02 +0800

    Update install.sh

--- raspberrypi/aria2/install.sh
+++ raspberrypi/aria2/install.sh
@@ -5,16 +5,13 @@ sudo apt install git -y
 sudo mkdir /etc/aria2-webui
 sudo git clone https://github.com/ziahamza/webui-aria2.git /etc/aria2-webui
 sudo killall aria2c
-sudo mkdir /etc/aria2 -p
-sudo wget -O /etc/aria2/aria2.conf https://raw.githubusercontent.com/jjye93/config-file/refs/heads/main/raspberrypi/aria2/aria2.conf
+sudo wget -O /etc/aria2-webui/aria2.conf https://raw.githubusercontent.com/jjye93/config-file/refs/heads/main/raspberrypi/aria2/aria2.conf
 sudo chmod 755 /etc/aria2/aria2.conf
-sudo wget -O /etc/systemd/system/aria2c.service https://raw.githubusercontent.com/jjye93/config-file/refs/heads/main/raspberrypi/aria2/aria2c.service
-sudo chmod 755 /etc/systemd/system/aria2c.service
-sudo systemctl enable aria2c
-sudo systemctl start aria2c
-git clone https://github.com/ziahamza/webui-aria2.git
-sudo mv webui-aria2 /etc/
-cd /etc/webui-aria2
+sudo wget -O /etc/systemd/system/aria2.service https://raw.githubusercontent.com/jjye93/config-file/refs/heads/main/raspberrypi/aria2/aria2.service
+sudo chmod 755 /etc/systemd/system/aria2.service
+sudo systemctl enable aria2
+sudo systemctl start aria2
+cd /etc/aria2-webui
 sudo apt install npm -y
 sudo npm install -g pm2
 sudo env PATH=$PATH:/usr/bin /usr/local/lib/node_modules/pm2/bin/pm2 startup systemd -u admin --hp /home/admin
