c9357e6ad5f3b166373d37d9803c7c9a99761204
jjye93 <89705425+jjye93@users.noreply.github.com>
2025-02-19 02:15:12 +0800

    Update docker-run.sh

--- raspberrypi/pi.local/docker-run.sh
+++ raspberrypi/pi.local/docker-run.sh
@@ -16,6 +16,7 @@ echo "----------------------------------------------------------------"
 sudo docker run -d --name alist --restart always -v '/etc/alist:/opt/alist/data' -v '/etc/alist/data:/data' -p '5244:5244' -p '5245:5245' -e 'PUID=1000' -e 'PGID=1000' -e 'TZ=Asia/Kuala_Lumpur' -e 'UMASK=022' xhofe/alist:latest
 sleep 10s
 sudo docker exec -it alist ./alist admin set qwer1234
+sudo apt-get update && sudo apt-get full-upgrade -y && sudo apt autoremove -y && sudo apt-get autoclean -y
 echo "----------------------------------------------------------------"
 echo "Completed"
 echo "----------------------------------------------------------------"
@@ -26,7 +27,6 @@ echo "transmission; http://pi.local:9091 with id admin and password qwer1234"
 echo "qbittorent; http://pi.local:8081"
 echo "alist; http://pi.local:5244 with id admin and password qwer1234"
 echo "plex media server; http://pi.local:32400/web"
-sudo apt-get update && sudo apt-get full-upgrade -y
 echo "----------------------------------------------------------------"
 echo "please perform a reboot"
 echo "----------------------------------------------------------------"
