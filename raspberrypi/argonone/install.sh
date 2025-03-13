8d679e31e2bc3c936881f3386c10b5babb35e295
jjye93 <89705425+jjye93@users.noreply.github.com>
2025-03-14 00:58:28 +0800

    Delete raspberrypi/argonone directory

--- raspberrypi/argonone/install.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/bash
-echo "argon-one"
-curl https://download.argon40.com/argon-eeprom.sh | bash
-curl https://download.argon40.com/argon1.sh | bash 
-echo "argon-config"
-echo "complete"
