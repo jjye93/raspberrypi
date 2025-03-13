1a33b0dad38411d3e30175f604dce4a92c893749
jjye93 <89705425+jjye93@users.noreply.github.com>
2025-03-14 01:49:02 +0800

    Update install.sh

--- smart home/mosquitto/install.sh
+++ smart home/mosquitto/install.sh
@@ -1,11 +1,19 @@
 #!/usr/bin/bash
 echo "mosquitto"
-sudo apt-get update && sudo apt-get full-upgrade -y
+sudo apt-get update && sudo apt-get full-upgrade -y && sudo apt-get install expect -y
 sudo apt-get install mosquitto -y
+expect <<EOF
+spawn sudo mosquitto_passwd -c /etc/mosquitto/passwd admin
+expect "Password: "
+send "qwer1234\r"
+expect "Reenter password: "
+send "qwer1234\r"
+expect eof
+EOF
 sudo systemctl stop mosquitto
-sudo mkdir -p /opt/mosquitto
-sudo wget -o /opt/mosquitto/mosquitto.conf https://raw.githubusercontent.com/jjye93/config-file/refs/heads/main/smart%20home/mosquitto/mosquitto.conf
-sudo chmod 777 /opt/mosquitto/mosquitto.conf
+sudo mv /etc/mosquitto/mosquitto.conf /etc/mosquitto/mosquitto.conf.1
+sudo wget -o /etc/mosquitto/mosquitto.conf https://raw.githubusercontent.com/jjye93/config-file/refs/heads/main/smart%20home/mosquitto/mosquitto.conf
+sudo chmod 777 /etc/mosquitto/mosquitto.conf
+sudo systemctl start mosquitto.conf
 sudo systemctl enable mosquitto.conf
-mosquitto -v
 echo "complete"
