9b6ac32e57d63c49312dac870563bcd9da9d336e
jjye93 <89705425+jjye93@users.noreply.github.com>
2025-03-14 01:30:20 +0800

    Update install.sh

--- raspberrypi/ssh/install.sh
+++ raspberrypi/ssh/install.sh
@@ -9,15 +9,15 @@ fi
 expect <<EOF
 spawn sudo passwd root
 expect "New password:"
-send "admin\r"
+send "qwer1234\r"
 expect "Retype new password:"
-send "admin\r"
+send "qwer1234\r"
 expect eof
 EOF
 sudo systemctl stop ssh
-sudo mv /etc/ssh/sshd_config /etc/ssh/sshd_config.backup
+sudo mv /etc/ssh/sshd_config /etc/ssh/sshd_config.1
 sudo wget -O /etc/ssh/sshd_config https://raw.githubusercontent.com/jjye93/config-file/refs/heads/main/raspberrypi/ssh/sshd_config
 sudo chmod 644 /etc/ssh/sshd_config 
-sudo chown root:root /etc/ssh/sshd_config 
 sudo systemctl start ssh
 echo "complete"
+return
