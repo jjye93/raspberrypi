81f3d8711728d92db47d110c14b009939a21d115
jjye93 <89705425+jjye93@users.noreply.github.com>
2025-03-14 21:05:19 +0800

    Create install.sh

---
+++ raspberrypi/prowlarr/install.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/bash
+sudo apt install curl sqlite3 -y
+wget --content-disposition 'http://prowlarr.servarr.com/v1/update/master/updatefile?os=linux&runtime=netcore&arch=arm64'
+tar -xvzf Prowlarr*.linux*.tar.gz
+sudo mv Prowlarr/ /etc
+sudo chown $USER:$USER -R /etc/Prowlarr
+cat << EOF | sudo tee /etc/systemd/system/prowlarr.service > /dev/null
+[Unit]
+Description=Prowlarr Daemon
+After=syslog.target network.target
+
+[Service]
+Type=simple
+ExecStart=/etc/Prowlarr/Prowlarr -nobrowser -data=/var/lib/prowlarr/
+TimeoutStopSec=20
+KillMode=process
+Restart=on-failure
+
+[Install]
+WantedBy=multi-user.target
+EOF
+sudo systemctl -q daemon-reload
+sudo systemctl enable --now -q prowlarr
+rm Prowlarr*.linux*.tar.gz
+return
