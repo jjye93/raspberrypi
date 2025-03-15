17298e69f92491c5ea7512812cf1ead4eac0f25b
jjye93 <89705425+jjye93@users.noreply.github.com>
2025-03-15 14:33:51 +0800

    Update install.sh

--- smart home/zigbee2mqtt/install.sh
+++ smart home/zigbee2mqtt/install.sh
@@ -5,9 +5,8 @@ sudo curl -fsSL https://deb.nodesource.com/setup_20.x | sudo -E bash -
 sudo apt-get install -y nodejs git make g++ gcc libsystemd-dev
 sudo npm install -g pnpm
 sudo mkdir /etc/zigbee2mqtt
-sudo chown -R ${USER}: /etc/zigbee2mqtt
 git clone --depth 1 https://github.com/Koenkk/zigbee2mqtt.git /etc/zigbee2mqtt
-cd /etc/zigbee2mqtt
+sudo chown -R admin: /etc/zigbee2mqtt && sudo chmod -R 777 /etc/zigbee2mqtt && cd /etc/zigbee2mqtt
 pnpm i --frozen-lockfile
 pnpm run build
 
