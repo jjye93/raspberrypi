0ade95d18eb7238ddf58339dc140668b68de57c3
jjye93 <89705425+jjye93@users.noreply.github.com>
2025-03-15 14:12:26 +0800

    Create install.sh

---
+++ raspberrypi/homebridge/install.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/bash
+curl -sSfL https://repo.homebridge.io/KEY.gpg | sudo gpg --dearmor | sudo tee /usr/share/keyrings/homebridge.gpg  > /dev/null
+echo "deb [signed-by=/usr/share/keyrings/homebridge.gpg] https://repo.homebridge.io stable main" | sudo tee /etc/apt/sources.list.d/homebridge.list > /dev/null
+sudo apt-get update && sudo apt-get install homebridge -y
+echo "Completed"
