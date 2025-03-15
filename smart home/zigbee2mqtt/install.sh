fe2e4fb395af1b21657582ccf444d87f321ddeab
jjye93 <89705425+jjye93@users.noreply.github.com>
2025-03-15 14:22:13 +0800

    Update install.sh

--- smart home/zigbee2mqtt/install.sh
+++ smart home/zigbee2mqtt/install.sh
@@ -10,7 +10,32 @@ git clone --depth 1 https://github.com/Koenkk/zigbee2mqtt.git /etc/zigbee2mqtt
 cd /etc/zigbee2mqtt
 pnpm i --frozen-lockfile
 pnpm run build
-sudo wget -O /etc/zigbee2mqtt/data/configuration.yaml https://raw.githubusercontent.com/jjye93/config-file/refs/heads/main/smart%20home/zigbee2mqtt/configuration.yaml
+
+cat << EOF | sudo tee /etc/zigbee2mqtt/data/configuration.yaml > /dev/null
+version: 4
+homeassistant:
+    enabled: false
+frontend:
+    enabled: true
+    port: 9699
+mqtt:
+    base_topic: zigbee2mqtt
+    server: 'mqtt://localhost:1883'
+    user: admin
+    password: qwer1234
+serial:
+    port: /dev/ttyUSB0
+    adapter: ember
+availability:
+    enable: true
+advance:
+    transmit_power: 20
+    baudrate: 115200
+    network_key: GENERATE
+    pan_id: GENERATE
+    ext_pan_id: GENERATE
+EOF
+
 cat << EOF | sudo tee /etc/systemd/system/zigbee2mqtt.service > /dev/null
 [Unit]
 Description=zigbee2mqtt
