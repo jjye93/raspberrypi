72770f57fc11fb2669cec6b16df8a1ffaba4bd21
jjye93 <89705425+jjye93@users.noreply.github.com>
2025-03-16 01:05:40 +0800

    Create docker-compose.sh

---
+++ docker-compose/docker-compose.sh
@@ -0,0 +1,81 @@
+#！/usr/bin/bash
+
+echo "Docker Compose Script"
+echo "Please select application for docker compose to install"
+
+pause_and_return() {
+    echo "Do you want to continue?"
+    select next_step in "Yes" "No"; do
+        case $REPLY in
+            1) break ;;  # Just break the select, returning to the main loop
+            2) echo "Exiting..."; exit 0 ;;
+            *) echo "Invalid selection. Try again." ;;
+        esac
+    done
+}
+
+docker() {
+    echo "Installing Docker"
+    bash -c "$(curl -fsSL https://raw.githubusercontent.com/jjye93/raspberrypi/refs/heads/main/install-script/docker/install.sh)"
+    echo "Completed"
+    pause_and_return
+}
+
+flaresolverr() {
+    echo "Install Flaresolverr"
+    sudo mkdir -p ~/Docker/flaresolverr
+    sudo wget -O ~/Docker/flaresolverr/compose.yml  https://raw.githubusercontent.com/jjye93/raspberrypi/refs/heads/main/docker-compose/flaresolverr/compose.yml
+    cd ~/Docker/flaresolverr/
+    docker compose up -d
+    echo "completed"
+    pause_and_return
+}
+
+photoprism() {
+    echo "Install Photoprism"
+    sudo mkdir -p ~/Docker/photoprism
+    sudo wget -O ~/Docker/photoprism/compose.yml  https://raw.githubusercontent.com/jjye93/raspberrypi/refs/heads/main/docker-compose/photoprism/compose.yml
+    cd ~/Docker/photoprism/
+    docker compose up -d
+    echo "completed"
+    pause_and_return
+}
+
+portainer() {
+    echo "Install portainer"
+    sudo mkdir -p ~/Docker/portainer
+    sudo wget -O ~/Docker/portainer/compose.yml  https://raw.githubusercontent.com/jjye93/raspberrypi/refs/heads/main/docker-compose/portainer/compose.yml
+    cd ~/Docker/portainer/
+    docker compose up -d
+    echo "completed"
+    pause_and_return
+
+}
+
+qbittorrent() {
+    echo "Install qbittorrent"
+    sudo mkdir -p ~/Docker/qbittorrent
+    sudo wget -O ~/Docker/qbittorrent/compose.yml  https://raw.githubusercontent.com/jjye93/raspberrypi/refs/heads/main/docker-compose/qbittorrent/compose.yml
+    cd ~/Docker/qbittorrent/
+    docker compose up -d
+    echo "completed"
+    pause_and_return    
+}
+
+PS3="Select application to run: "
+options=("Docker" "FlareSolverr" "PhotoPrism" "Portainer" "qBittorrent" "Exit")
+
+while true; do
+    select choice in "${options[@]}"; do
+        case $REPLY in
+            1) docker ;;
+            2) flaresolverr ;;
+            3) photoprism ;;
+            4) portainer ;;
+            5) qbittorrent ;;
+            6) echo "Exiting..."; exit 0 ;;
+            *) echo "Invalid selection. Try again." ;;
+        esac
+        break
+    done
+done
