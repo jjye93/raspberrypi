fc44a7dd7228735676fbd40254af36160e738d86
jjye93 <89705425+jjye93@users.noreply.github.com>
2025-03-15 22:41:21 +0800

    Delete install-script/portainer directory

--- install-script/portainer/install.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/bash
-mkdir -p ~/Docker/portainer
-wget -O ~/Docker/portainer/compose.yml https://raw.githubusercontent.com/jjye93/config-file/refs/heads/main/raspberrypi/portainer/docker-compose.yml
-chmod +x ~/Docker/portainer/compose.yml
-cd ~/Docker/portainer
-sudo docker compose up -d
-echo "Complete"
