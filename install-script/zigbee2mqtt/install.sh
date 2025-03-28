9f41aaef37832aeb4b474309e7cac275150ad1c5
jjye93 <89705425+jjye93@users.noreply.github.com>
2025-03-29 01:53:04 +0800

    Update install.sh

--- install-script/zigbee2mqtt/install.sh
+++ install-script/zigbee2mqtt/install.sh
@@ -2,6 +2,7 @@
 echo "install mosquitto"
 bash -c "$(curl -fsSL https://raw.githubusercontent.com/jjye93/raspberrypi/refs/heads/main/install-script/zigbee2mqtt/mosquitto/install.sh)"
 echo "configured mosquitto with username "admin" and password "qwer1234""
+wait 10s
 echo "install zigbee2mqtt"
 bash -c "$(curl -fsSL https://raw.githubusercontent.com/jjye93/raspberrypi/refs/heads/main/install-script/zigbee2mqtt/zigbee2mqtt/install.sh)"
 echo "zigbee2mqtt are hosted on port 9699"
