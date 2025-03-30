100f8ec99eb4d37fd428d27218d7cbad7ff5ca55
jjye93 <89705425+jjye93@users.noreply.github.com>
2025-03-31 07:29:23 +0800

    new file:   smarthome/matterbridge/docker-compose/compose.yml 	new file:   smarthome/matterbridge/node/install.sh 	modified:   smarthome/smarthome.sh

--- smarthome/smarthome.sh
+++ smarthome/smarthome.sh
@@ -14,12 +14,50 @@ pause_and_return() {
     done
 }
 
+docker() {
+    bash -c "$(curl -fsSL https://raw.githubusercontent.com/jjye93/raspberrypi/refs/heads/main/install-script/docker/install.sh)"
+    echo "Completed"
+}
 HomeBridge() {
     echo "Installing HomeBridge"
     curl -sSfL https://repo.homebridge.io/KEY.gpg | sudo gpg --dearmor | sudo tee /usr/share/keyrings/homebridge.gpg  > /dev/null
     echo "deb [signed-by=/usr/share/keyrings/homebridge.gpg] https://repo.homebridge.io stable main" | sudo tee /etc/apt/sources.list.d/homebridge.list > /dev/null
     sudo apt-get install homebridge -y
-    echo "Completed"    
+    echo "Completed" 
+    pause_and_return   
+}
+
+Zigbee2MQTT() {
+    echo "Installing Zigbee2MQTT"
+    bash -c "$(curl -fsSL https://raw.githubusercontent.com/jjye93/raspberrypi/refs/heads/main/smarthome/zigbee2mqtt/install.sh)"
+    echo "Completed"
+    pause_and_return
+}
+
+NodeRed() {
+    echo "Installing NodeRed..."
+    bash <(curl -fsSL https://raw.githubusercontent.com/node-red/linux-installers/master/deb/update-nodejs-and-nodered)
+    echo "Completed"
+    pause_and_retirn
+}
+
+HomeAssistance() {
+    if command -v docker &> /dev/null; then
+        echo "Docker is already installed."
+    else
+        echo "Docker not found. Installing..."
+        docker
+    fi
+    sudo mkdir -p /etc/homeassistant
+    sudo wget -O /etc/homeassistant/compose.yml  https://raw.githubusercontent.com/jjye93/raspberrypi/refs/heads/main/docker-compose/homeassistant/compose.yml
+    sudo docker compose -f /etc/homeassistant/compose.yml up -d
+    echo "completed"
+    pause_and_return  
+}
+
+MatterBridge() {
+    echo "Installing Matterbridge"
+    
 }
 
 PS3="Select your package: "
