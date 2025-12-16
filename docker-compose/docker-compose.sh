d61d2c67586abdc6946a1158059893181ba7ad4e
jjye93 <89705425+jjye93@users.noreply.github.com>
2025-12-16 22:11:50 +0800

    Update docker-compose.sh

--- docker-compose/docker-compose.sh
+++ docker-compose/docker-compose.sh
@@ -16,34 +16,34 @@ pause_and_return() {
 
 install_docker() {
     echo "Installing Docker..."
-    bash -c "$(curl -fsSL https://raw.githubusercontent.com/jjye93/raspberrypi/refs/heads/main/install-script/docker/install.sh)"
+    bash -c "$(curl -fsSL https://get.docker.com)"
     echo "Completed"
     pause_and_return
 }
 
 flaresolverr() {
     echo "Install Flaresolverr"
-    sudo mkdir -p /etc/flaresolverr
-    sudo git clone -O /etc/flaresolverr https://github.com/FlareSolverr/FlareSolverr.git
-    sudo docker compose -f /etc/flaresolverr/docker-compose.yml up -d
+    sudo mkdir -p ~/Applications/flaresolverr
+    sudo git clone -O ~/Applications/flaresolverr https://github.com/FlareSolverr/FlareSolverr.git
+    sudo docker compose -f ~/Applications/flaresolverr/docker-compose.yml up -d
     echo "completed"
     pause_and_return
 }
 
 photoprism() {
     echo "Install Photoprism"
-    sudo mkdir -p /etc/photoprism
-    sudo wget -O /etc/photoprism/compose.yml  https://raw.githubusercontent.com/jjye93/raspberrypi/refs/heads/main/docker-compose/photoprism/compose.yml
-    sudo docker compose -f /etc/photoprism/compose.yml up -d
+    sudo mkdir -p ~/Applications/photoprism
+    sudo wget -O ~/Applications/photoprism/compose.yml  https://raw.githubusercontent.com/jjye93/raspberrypi/refs/heads/main/docker-compose/photoprism/compose.yml
+    sudo docker compose -f ~/Applications/photoprism/compose.yml up -d
     echo "completed"
     pause_and_return
 }
 
 portainer() {
     echo "Install portainer"
-    sudo mkdir -p /etc/portainer
-    sudo wget -O /etc/portainer/compose.yml  https://raw.githubusercontent.com/jjye93/raspberrypi/refs/heads/main/docker-compose/portainer/compose.yml
-    sudo docker compose -f /etc/portainer/compose.yml up -d
+    sudo mkdir -p ~/Applications/portainer
+    sudo wget -O ~/Applications/portainer/compose.yml  https://raw.githubusercontent.com/jjye93/raspberrypi/refs/heads/main/docker-compose/portainer/compose.yml
+    sudo docker compose -f ~/Applications/portainer/compose.yml up -d
     echo "completed"
     pause_and_return
 
@@ -51,34 +51,43 @@ portainer() {
 
 homeassistant() {
     echo "Install homeassistant"
-    sudo mkdir -p /etc/homeassistant
-    sudo wget -O /etc/homeassistant/compose.yml  https://raw.githubusercontent.com/jjye93/raspberrypi/refs/heads/main/docker-compose/homeassistant/compose.yml
-    sudo docker compose -f /etc/homeassistant/compose.yml up -d
+    sudo mkdir -p ~/Applications/homeassistant
+    sudo wget -O ~/Applications/homeassistant/compose.yml  https://raw.githubusercontent.com/jjye93/raspberrypi/refs/heads/main/docker-compose/homeassistant/compose.yml
+    sudo docker compose -f ~/Applications/homeassistant/compose.yml up -d
     echo "completed"
     pause_and_return    
 }
 
 ani-rss() {
     echo "Install ani-rss"
-    sudo mkdir -p /etc/ani-rss
-    sudo wget -O /etc/ani-rss/compose.yml https://raw.githubusercontent.com/jjye93/raspberrypi/refs/heads/main/docker-compose/Ani-rss/compose.yml
-    sudo docker compose -f /etc/ani-rss/compose.yml up -d
+    sudo mkdir -p ~/Applications/ani-rss
+    sudo wget -O ~/Applications/ani-rss/compose.yml https://raw.githubusercontent.com/jjye93/raspberrypi/refs/heads/main/docker-compose/Ani-rss/compose.yml
+    sudo docker compose -f ~/Applications/ani-rss/compose.yml up -d
     echo "completed"
     pause_and_return
 }
 
 immich() {
     echo "Install immich"
-    sudo mkdir -p /etc/immich
-    sudo wget -O /etc/immich/compose.yml https://raw.githubusercontent.com/jjye93/raspberrypi/refs/heads/main/docker-compose/immich/compose.yml
-    sudo wget -O /etc/immich/.env https://raw.githubusercontent.com/jjye93/raspberrypi/refs/heads/main/docker-compose/immich/.env
-    sudo docker compose -f /etc/immich/compose.yml up -d
+    sudo mkdir -p ~/Applications/immich
+    sudo wget -O ~/Applications/immich/compose.yml https://raw.githubusercontent.com/jjye93/raspberrypi/refs/heads/main/docker-compose/immich/compose.yml
+    sudo wget -O ~/Applications/immich/.env https://raw.githubusercontent.com/jjye93/raspberrypi/refs/heads/main/docker-compose/immich/.env
+    sudo docker compose -f ~/Applications/immich/compose.yml up -d
+    echo "completed"
+    pause_and-return
+}
+
+kvideo() {
+    echo "Install Kvideo"
+    sudo mkdir -p ~/Applications/kvideo
+    sudo wget -O ~/Applications/kvideo/compose.yml https://raw.githubusercontent.com/jjye93/raspberrypi/refs/heads/main/docker-compose/kvideo/compose.yml
+    sudo docker compose -f ~/Applications/kvideo/compose.yml up -d
     echo "completed"
     pause_and-return
 }
 
 PS3="Select application to run: "
-options=("Docker" "FlareSolverr" "PhotoPrism" "Portainer" "Home Assistant" "Ani-rss" "immich" "Exit")
+options=("Docker" "FlareSolverr" "PhotoPrism" "Portainer" "Home Assistant" "Ani-rss" "immich" "kvideo" "Exit")
 
 while true; do
     select choice in "${options[@]}"; do
@@ -90,7 +99,8 @@ while true; do
             5) homeassistant ;;
             6) ani-rss ;;
             7) immich ;;
-            8) echo "Exiting..."; exit 0 ;;
+            8) kvideo ;;
+            9) echo "Exiting..."; exit 0 ;;
             *) echo "Invalid selection. Try again." ;;
         esac
         break
